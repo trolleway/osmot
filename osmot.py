a7888be839479b8fa1f3c3b88c41f6a83e45fe36
Artem Svetlov <trolleway@yandex.ru>
2016-05-16 18:17:57 +0300

    Update osmot.py

--- osmot.py
+++ osmot.py
@@ -528,7 +528,7 @@ CREATE  table terminals_export AS
         '''
         CREATE OR REPLACE VIEW routes_with_refs AS 
         (SELECT
-        ROW_NUMBER() OVER() 				AS id,
+        ROW_NUMBER() OVER() 				AS road_id,
         degrees(ST_azimuth(ST_Line_Interpolate_Point(way,0.5),ST_Line_Interpolate_Point(way,0.501)))+0 AS angle,
         ST_X(ST_Line_Interpolate_Point(way,0.5)) 	AS x,
         ST_Y(ST_Line_Interpolate_Point(way,0.5)) 	AS y,
