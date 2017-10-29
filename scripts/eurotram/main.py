2c4712bfda7647095c52fe254378b77740944c3b
Artem Svetlov <trolleway@yandex.ru>
2017-10-29 14:59:43 +0300

    refactoring

--- scripts/eurotram/main.py
+++ scripts/eurotram/main.py
@@ -7,8 +7,32 @@
 
 import os
 
+def argparser_prepare():
+
+    class PrettyFormatter(argparse.ArgumentDefaultsHelpFormatter,
+        argparse.RawDescriptionHelpFormatter):
+
+        max_help_position = 35
+
+    parser = argparse.ArgumentParser(description='',
+            formatter_class=PrettyFormatter)
+    parser.add_argument('-u', '--update', type=str, choices=['day', 'hour', 'minute'] default='day',
+                        help='command for osmupdate')
+
+    parser.epilog = \
+        '''Samples:
+%(prog)s
+''' \
+        % {'prog': parser.prog}
+    return parser
+
 #if prevdump not exists - download CFO from geofabrik and crop to Europe
 def updateDump():
+    
+    parser = argparser_prepare()
+    args = parser.parse_args()
+
+    update = args.update
     
     dump_url='http://download.geofabrik.de/europe-latest.osm.pbf'
     downloaded_dump='europe-latest.osm.pbf'
@@ -26,7 +50,11 @@ def updateDump():
         os.rename(downloaded_dump, work_dump) 
 
     #if prevdump dump exists - run osmupdate, it updating it to last hour state with clipping, and save as currentdump
-    cmd='osmupdate '+ work_dump + ' ' + updated_dump + ' --hour   -v -B='+poly_file #--day --hour 
+    cmd = 'osmupdate {work_dump}  {updated_dump} --{update}   -v -B={poly_file}'.format(
+        work_dump=work_dump,
+        updated_dump=updated_dump,
+        update=update,
+        poly_file=poly_file)
     os.system(cmd)
     
     #if osmupdate not find updates in internet - new file not created, will be used downloaded file
