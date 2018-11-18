abb6f48e37b22ebe1d70543e7561a139fdc42a5c
Artem Svetlov <trolleway@yandex.ru>
2018-11-18 23:44:56 +0300

    Moscow oblast

---
+++ scripts/mosobl/config.example.py
@@ -0,0 +1,7 @@
+dbname='gis'
+user='trolleway'
+host='localhost'
+password='admin'
+
+dump_url='http://download.geofabrik.de/europe/latvia-latest.osm.pbf'
+poly_url='http://download.geofabrik.de/europe/latvia.poly'
