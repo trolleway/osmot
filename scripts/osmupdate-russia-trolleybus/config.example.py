454160f362b96538717e6577ceb658ed8f8dae1a
Artem Svetlov <trolleway@yandex.ru>
2018-11-22 02:53:09 +0300

    russia trolleybus

---
+++ scripts/osmupdate-russia-trolleybus/config.example.py
@@ -0,0 +1,7 @@
+dbname='gis'
+user='trolleway'
+host='localhost'
+password='admin'
+
+dump_url='http://download.geofabrik.de/russia-latest.osm.pbf'
+poly_url='http://download.geofabrik.de/russia.poly'
---
+++ scripts/osmupdate-russia-trolleybus/readme.md
@@ -0,0 +1,3 @@
+Use wget+osmupdate for up-to-time map of tram or trolleybus of whole country. Not work with bus, because all routes should not intercect with boundary
+
+works on windows
