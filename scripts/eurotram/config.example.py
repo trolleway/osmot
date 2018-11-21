f713537a94659f0b2b77f7b6ca5b03a66a025ef4
Artem Svetlov <trolleway@yandex.ru>
2018-11-22 00:13:41 +0300

    update example

--- scripts/eurotram/config.example.py
+++
@@ -1,7 +0,0 @@
-dbname='gis'
-user='trolleway'
-host='localhost'
-password='admin'
-
-dump_url='http://download.geofabrik.de/europe/latvia-latest.osm.pbf'
-poly_url='http://download.geofabrik.de/europe/latvia.poly'
