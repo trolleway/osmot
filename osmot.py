dfa819dd2dc954d67ac95d34bd4ec6ebb858587c
Artem Svetlov <trolleway@yandex.ru>
2020-01-31 02:01:37 +0300

    Update osmot.py

--- osmot.py
+++ osmot.py
@@ -9,7 +9,7 @@ import sys
 import logging
 
 
-logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(levelname)-8s %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(level=logging.INFO,format='%(asctime)s %(levelname)-8s %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
 logger.info('Start')
